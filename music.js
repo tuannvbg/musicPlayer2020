f32ca9c067bb29a6226a1d2296ec546e90ae049c
tuannvbg <nguyentuan2005@gmail.com>
2020-11-24 02:51:48 +0700

    Update music.js

--- music.js
+++ music.js
@@ -1,53 +1,53 @@
 const playlist= [
     {
         songName : "alan-walker-alone",
-        songImg : "/images/alanWalker.jpg",
-        songSrc : "/music/alan-walker-alone.mp3"
+        songImg : "images/alanWalker.jpg",
+        songSrc : "music/alan-walker-alone.mp3"
     },
     {
         songName : "apna-time-aayega-gully-boy",
-        songImg : "/images/apnaTime.jpg",
-        songSrc : "/music/apna-time-aayega-gully-boy.mp3"
+        songImg : "images/apnaTime.jpg",
+        songSrc : "music/apna-time-aayega-gully-boy.mp3"
     },
     {
         songName : "Sanu Ek Pal Chain - Raid",
-        songImg : "/images/sanuEkPal.jpg",
-        songSrc : "/music/Sanu Ek Pal Chain - Raid.mp3"
+        songImg : "images/sanuEkPal.jpg",
+        songSrc : "music/Sanu Ek Pal Chain - Raid.mp3"
     },
     {
         songName : "jitni-dafa-dekhu-tujhe",
-        songImg : "/images/jitniDafa.png",
-        songSrc : "/music/jitni-dafa-dekhu-tujhe.mp3"
+        songImg : "images/jitniDafa.png",
+        songSrc : "music/jitni-dafa-dekhu-tujhe.mp3"
     },
     {
         songName : "love-me-like-you-do",
-        songImg : "/images/Love Me Like You Do.png",
-        songSrc : "/music/love-me-like-you-do.mp3"
+        songImg : "images/Love Me Like You Do.png",
+        songSrc : "music/love-me-like-you-do.mp3"
     },
     {
         songName : "Post Malone - Hollywood s Bleeding (Audio)",
-        songImg : "/images/hollywood.jpg",
-        songSrc : "/music/Post Malone - Hollywood s Bleeding (Audio).mp3"
+        songImg : "images/hollywood.jpg",
+        songSrc : "music/Post Malone - Hollywood s Bleeding (Audio).mp3"
     },
     {
         songName : "roi-na-je-yaad-meri-aayi-ve",
-        songImg : "/images/royiNa.jpg",
-        songSrc : "/music/roi-na-je-yaad-meri-aayi-ve.mp3"
+        songImg : "images/royiNa.jpg",
+        songSrc : "music/roi-na-je-yaad-meri-aayi-ve.mp3"
     },
     {
         songName : "The Heart Wants What It Wants (Official Video) (1)",
-        songImg : "/images/selena-gomez1.jpg",
-        songSrc : "/music/Selena Gomez - The Heart Wants What It Wants (Official Video) (1).mp3"
+        songImg : "images/selena-gomez1.jpg",
+        songSrc : "music/Selena Gomez - The Heart Wants What It Wants (Official Video) (1).mp3"
     },
     {
         songName : "tere-jaisa-yaar-kahan-new-version",
-        songImg : "/images/tereJaisa.jpg",
-        songSrc : "/music/tere-jaisa-yaar-kahan-new-version.mp3"
+        songImg : "images/tereJaisa.jpg",
+        songSrc : "music/tere-jaisa-yaar-kahan-new-version.mp3"
     },
     {
         songName : "Bon Appetit",
-        songImg : "/images/bonApetit.jpg",
-        songSrc : "/music/Bon Appetit.mp3"
+        songImg : "images/bonApetit.jpg",
+        songSrc : "music/Bon Appetit.mp3"
     }
 ]
 
